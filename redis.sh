e9d3cb32f85f66268704f5df1f4b3674728534d1
zgrguric <zgrguric@xrplwin.com>
2023-11-11 15:46:17 +0100

    Create redis.sh

---
+++ redis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# REDIS
+###########################
+
+base=/root/deploy
+
+apt install -y redis
+
+# backup original
+cp /etc/redis/redis.conf /etc/redis/redis.backup
+
+# copy optimized ( https://www.linode.com/docs/databases/redis/install-and-configure-redis-on-centos-7 )
+cp $base/templates/redis/redis.conf /etc/redis/redis.conf
+
+systemctl enable redis
+systemctl start redis
+
+# fine tuning
+sysctl vm.overcommit_memory=1
+
+echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
+
+
+# PHP-REDIS
+###########################
+#yum --enablerepo=epel -y install php-pecl-redis
