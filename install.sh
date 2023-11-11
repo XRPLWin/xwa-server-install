a4d58f83311fa32f6b0e0eb90e35f252942ddda6
zgrguric <zgrguric@xrplwin.com>
2023-11-11 12:13:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 echo 'XWA Installer: Percona MyRocks'
 
+#see https://docs.percona.com/percona-server/8.0/install-myrocks.html#install-percona-myrocks
+
 sudo apt update
 sudo apt install curl
 curl -O https://repo.percona.com/apt/percona-release_latest.generic_all.deb
@@ -10,14 +12,17 @@ sudo apt update
 
 percona-release setup ps80
 sudo apt install -y percona-server-rocksdb
-# USER INPUT: You will be asked to enter mysql root password then "Use Strong Encryption"
+# USER INPUT: You will be asked to enter mysql root password (in this example MyPASSWORD) then "Use Strong Encryption"
+# My location: /etc/mysql/my.cnf
+# Symlink: /etc/systemd/system/multi-user.target.wants/mysql.service → /lib/systemd/system/mysql.service
+
+# Enable myrocks:
+# ps-admin --enable-rocksdb -u <mysql_admin_user> -p[mysql_admin_pass] [-S <socket>] [-h <host> -P <port>]
+ps-admin --enable-rocksdb -u root -pMyPASSWORD
 
 
 
-
-
-
-
+# OLD BELOW
 yum -y update
 yum -y install epel-release nano net-tools openssh-server which unzip tar
 
