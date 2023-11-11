ab48f09f74d48001ef7c4bf583cea909a76f3d31
zgrguric <zgrguric@xrplwin.com>
2023-11-11 16:03:53 +0100

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -13,7 +13,7 @@ cp /etc/redis/redis.conf /etc/redis/redis.backup
 # copy optimized ( https://www.linode.com/docs/databases/redis/install-and-configure-redis-on-centos-7 )
 cp $base/templates/redis/redis.conf /etc/redis/redis.conf
 
-systemctl enable redis
+#systemctl enable redis
 systemctl start redis
 
 # fine tuning
@@ -21,6 +21,7 @@ sysctl vm.overcommit_memory=1
 
 echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
 
+systemctl restart redis
 
 # PHP-REDIS
 ###########################
